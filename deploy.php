e75e6c9ac8e1da7ec10ea79a39fd91d41f6807c8
Ferdinand Malcher <ferdinand@malcher.media>
2021-06-05 20:07:31 +0200

    date

--- deploy.php
+++ deploy.php
@@ -1,4 +1,3 @@
 <?php
 echo shell_exec('/usr/bin/git pull 2>&1');
-// exec('/usr/bin/git pull', $out);
 ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<h1>Testseite</h1>
+
+Toller Test!
+
+<hr>
+
+<small><?php echo date('d.m.Y') ?></small>
