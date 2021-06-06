18c8d176713e5ce706286fcd60f47e9f689ecb7b
Lukas Wahl <git@subsample.de>
2021-06-06 18:13:00 +0200

    Token test

--- deploy.php
+++ deploy.php
@@ -1,7 +1,18 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    echo shell_exec('/usr/bin/git pull 2>&1');
+// In FinalVersion als Umgebungsvariable oder File
+$secret = '123';
+
+$token = $_POST['token'];
+$isValid = strcmp($secret, $token);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
+
+if (!$isValid) {
+    die();
+}
+
+echo shell_exec('/usr/bin/git pull 2>&1');
 
 ?>
